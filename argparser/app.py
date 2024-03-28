6d39534a0f2fadf80da2c50b08a312ea38ee0917
Hiroyuki Deguchi <deguchi.hiroyuki.db0@is.naist.jp>
2024-03-29 01:00:00 +0900

    Support ArgumentDefaultsHelpFormatter in newer python

--- argparser/app.py
+++ argparser/app.py
@@ -5,6 +5,7 @@ import sys
 from argparse import ArgumentParser, Namespace
 from typing import Any, Dict, Optional, Sequence
 
+
 class ArgumentParserWrapper(ArgumentParser):
     def _print_message(self, message, file=None):
         if message:
@@ -22,7 +23,52 @@ class HelpFormatter(
     argparse.MetavarTypeHelpFormatter,
     argparse.RawDescriptionHelpFormatter,
 ):
-    pass
+    def _format_action(self, action):
+        # determine the required width and the entry label
+        help_position = min(self._action_max_length + 2, self._max_help_position)
+        help_width = max(self._width - help_position, 11)
+        action_width = help_position - self._current_indent - 2
+        action_header = self._format_action_invocation(action)
+
+        # no help; start on same line and add a final newline
+        if not action.help:
+            tup = self._current_indent, "", action_header
+            action_header = "%*s%s\n" % tup
+
+        # short action name; start on the same line and pad two spaces
+        elif len(action_header) <= action_width:
+            tup = self._current_indent, "", action_width, action_header
+            action_header = "%*s%-*s  " % tup
+            indent_first = 0
+
+        # long action name; start on the next line
+        else:
+            tup = self._current_indent, "", action_header
+            action_header = "%*s%s\n" % tup
+            indent_first = help_position
+
+        # collect the pieces of the action help
+        parts = [action_header]
+
+        # if there was help for the action, add lines of help text
+        if action.help:
+            help_text = self._expand_help(action)
+            if help_text:
+                help_lines = self._split_lines(help_text, help_width)
+                parts.append("%*s%s\n" % (indent_first, "", help_lines[0]))
+                for line in help_lines[1:]:
+                    parts.append("%*s%s\n" % (help_position, "", line))
+
+        # or add a newline if the description doesn't end with one
+        elif not action_header.endswith("\n"):
+            parts.append("\n")
+
+        # if there are any sub-actions, add their help as well
+        for subaction in self._iter_indented_subactions(action):
+            parts.append(self._format_action(subaction))
+
+        # return a single string
+        return self._join_parts(parts)
 
 
 def strtobool(val: str) -> bool:
